605da66599b4669fb34f68e303e9ce0546dcbad4
Nargonath <jonas.pauthier@gmail.com>
2022-12-14 20:51:59 -0500

    :construction_worker: add semantic-release git plugin

--- .releaserc.js
+++ .releaserc.js
@@ -1,3 +1,14 @@
 module.exports = {
-  plugins: ["semantic-release-gitmoji", "@semantic-release/github"],
+  plugins: [
+    "semantic-release-gitmoji",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"],
+        message:
+          ":bookmark: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
 };
