ab971f0dff0a17a5d068e82bac2193c5f9a065c1
Nargonath <jonas.pauthier@gmail.com>
2022-12-18 09:26:46 -0500

    :bug: fix errors in lualine support

--- lua/lualine/themes/spaceduck.lua
+++ lua/lualine/themes/spaceduck.lua
@@ -1,34 +1,35 @@
+local colors = require("palette")
 local spaceduck = {}
 
 spaceduck.normal = {
   -- gui parameter is optional and behaves the same way as in vim's highlight command
-  a = { bg = colors.grey_purple, fg = colors.blackish, gui = "bold" },
-  b = { bg = colors.dark_purple, fg = colors.greyer },
-  c = { bg = colors.blackish, fg = colors.greyer },
+  a = { bg = colors.grey_purple.hex, fg = colors.blackish.hex, gui = "bold" },
+  b = { bg = colors.dark_purple.hex, fg = colors.greyer.hex },
+  c = { bg = colors.blackish.hex, fg = colors.greyer.hex },
 }
 
 spaceduck.insert = {
-  a = { bg = colors.green, fg = colors.blackish, gui = "bold" },
-  b = { bg = colors.dark_purple, fg = colors.greyer },
-  c = { bg = colors.blackish, fg = colors.greyer },
+  a = { bg = colors.green.hex, fg = colors.blackish.hex, gui = "bold" },
+  b = { bg = colors.dark_purple.hex, fg = colors.greyer.hex },
+  c = { bg = colors.blackish.hex, fg = colors.greyer.hex },
 }
 
 spaceduck.visual = {
-  a = { bg = colors.yellow, fg = colors.blackish, gui = "bold" },
-  b = { bg = colors.dark_purple, fg = colors.greyer },
-  c = { bg = colors.blackish, fg = colors.greyer },
+  a = { bg = colors.yellow.hex, fg = colors.blackish.hex, gui = "bold" },
+  b = { bg = colors.dark_purple.hex, fg = colors.greyer.hex },
+  c = { bg = colors.blackish.hex, fg = colors.greyer.hex },
 }
 
 spaceduck.replace = {
-  a = { bg = colors.purple, fg = colors.blackish, gui = "bold" },
-  b = { bg = colors.dark_purple, fg = colors.greyer },
-  c = { bg = colors.blackish, fg = colors.greyer },
+  a = { bg = colors.purple.hex, fg = colors.blackish.hex, gui = "bold" },
+  b = { bg = colors.dark_purple.hex, fg = colors.greyer.hex },
+  c = { bg = colors.blackish.hex, fg = colors.greyer.hex },
 }
 
 spaceduck.command = {
-  a = { bg = colors.cyan, fg = colors.blackish, gui = "bold" },
-  b = { bg = colors.dark_purple, fg = colors.greyer },
-  c = { bg = colors.blackish, fg = colors.greyer },
+  a = { bg = colors.cyan.hex, fg = colors.blackish.hex, gui = "bold" },
+  b = { bg = colors.dark_purple.hex, fg = colors.greyer.hex },
+  c = { bg = colors.blackish.hex, fg = colors.greyer.hex },
 }
 
 -- you can assign one colorscheme to another, if a colorscheme is
@@ -36,9 +37,9 @@ spaceduck.command = {
 spaceduck.terminal = spaceduck.normal
 
 spaceduck.inactive = {
-  a = { bg = colors.blackish, fg = colors.greyer, gui = "bold" },
-  b = { bg = colors.blackish, fg = colors.greyer },
-  c = { bg = colors.blackish, fg = colors.greyer },
+  a = { bg = colors.blackish.hex, fg = colors.greyer.hex, gui = "bold" },
+  b = { bg = colors.blackish.hex, fg = colors.greyer.hex },
+  c = { bg = colors.blackish.hex, fg = colors.greyer.hex },
 }
 
 -- lualine.theme = spaceduck
